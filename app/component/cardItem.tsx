e149004dc3dded03ca32282a98187b5fd9a956c0
nunu111 <nunu111za@hotmail.com>
2024-09-14 16:00:34 +0700

    add popup

--- app/component/cardItem.tsx
+++ app/component/cardItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ItemPopup from './itemPopup';
 
 type detail ={
   Topic:string;
@@ -6,14 +7,20 @@ type detail ={
 }
 
 export default function CardItem(param: detail ) {
+
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
-    <div className="bg-white rounded-lg p-6 text-center">
-        <h2 className="text-xl font-bold">{param.Topic}</h2>
-        {param.Level == "Easy"? <p className="text-green-500">Easy</p> : param.Level == "Medium"? <p className="text-yellow-500">Medium</p> :<p className="text-red-500">Hard</p>}
-        
-        <button className="bg-[#5e4ae3] text-white py-2 px-4 mt-4 rounded-lg">
-          Start
-        </button>
+    <div>
+      {showModal? <ItemPopup usestate={setShowModal} />:null}
+      <div className="bg-white rounded-lg p-6 text-center">
+          <h2 className="text-xl font-bold">{param.Topic}</h2>
+          {param.Level == "Easy"? <p className="text-green-500">Easy</p> : param.Level == "Medium"? <p className="text-yellow-500">Medium</p> :<p className="text-red-500">Hard</p>}
+          
+          <button data-modal-target="default-modal" className="bg-[#5e4ae3] text-white py-2 px-4 mt-4 rounded-lg" onClick={()=> setShowModal(true)}>
+            Start
+          </button>
+      </div>
     </div>
   )
 }
