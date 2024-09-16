aff21d6d8c79f66132a433c0665da40ec5a95ca8
Sirawich <Sirawich_Pin@cmu.ac.th>
2024-09-16 22:04:25 +0700

    using carditem

--- app/component/cardItem.tsx
+++ app/component/cardItem.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 
-type detail ={
-  Topic:string;
-  Level:string;
-}
+type detail = {
+  Topic: string;
+  Level: string;
+  Category: string;
+};
 
-export default function CardItem(param: detail ) {
+export default function CardItem(param: detail) {
   return (
     <div className="bg-white rounded-lg p-6 text-center">
-        <h2 className="text-xl font-bold">{param.Topic}</h2>
-        {param.Level == "Easy"? <p className="text-green-500">Easy</p> : param.Level == "Medium"? <p className="text-yellow-500">Medium</p> :<p className="text-red-500">Hard</p>}
-        
-        <button className="bg-[#5e4ae3] text-white py-2 px-4 mt-4 rounded-lg">
-          Start
-        </button>
+      <h2 className="text-3xl font-bold">{param.Topic}</h2>
+      {param.Level == "Easy" ? (
+        <p className="text-green-500">Easy</p>
+      ) : param.Level == "Medium" ? (
+        <p className="text-yellow-500">Medium</p>
+      ) : (
+        <p className="text-red-500">Hard</p>
+      )}
+      <p className="text-gray-500 mt-2"> {param.Category}</p>
+      <button className="bg-[#5e4ae3] text-white py-2 px-4 mt-4 rounded-lg">
+        Start
+      </button>
     </div>
-  )
+  );
 }
