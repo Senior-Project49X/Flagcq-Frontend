2a56ea5092274e22eabb71771a1c476d6a20d7cd
nunu111 <nunu111za@hotmail.com>
2024-12-06 01:08:17 +0700

    add middleware

--- app/createQuestion/page.tsx
+++ app/createQuestion/page.tsx
@@ -6,7 +6,7 @@ import { CreateQuestionAPI } from "../lib/API/QuestionAPI";
 import LoadingPopup from "../component/LoadingPopup";
 import { log } from "console";
 
-interface typeOfNewData {
+interface CreateNewQuestion {
   CategoriesId: string | null;
   Title: string;
   Description: string;
@@ -14,22 +14,58 @@ interface typeOfNewData {
   Point: number;
   DifficultyId: string;
   FilePath: File | null;
-  Mode: string;
+  Mode: string[];
 }
-
+interface ButtonStates {
+  [key: string]: boolean; // Define a dynamic object where keys are strings and values are boolean
+}
 export default function CreateQuestion() {
+  const [modeSelection, setModeSelection] = useState<ButtonStates>({
+    Practice: false,
+    Tournament: false,
+    UnPublic: true,
+  });
+
   const [Loading, setLoading] = useState<boolean>(false);
   const [isFailed, setIsFailed] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [selectedTournament, setSelectedTournament] = useState<string[]>([]);
   const onCreateQuestion = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
+    formData.append("Practice", modeSelection.Practice.toString());
+    // formData.append("Tournament", []);
+    console.log(typeof formData.get("Practice"));
+
+    formData.append(`Tournament`, `[${selectedTournament.toString()}]`);
 
     CreateQuestionAPI(formData, { setIsFailed, setMessage, setIsSuccess });
 
     setLoading(true);
   };
+  const handleToggle = (buttonKey: string): void => {
+    if (buttonKey === "None") {
+      setModeSelection((prevStates) => ({
+        Practice: false,
+        Tournament: false,
+        UnPublic: true,
+      }));
+    } else if (buttonKey === "Tournament") {
+      setModeSelection((prevStates) => ({
+        Practice: false,
+        Tournament: true,
+        UnPublic: false,
+      }));
+    } else if (buttonKey === "Practice") {
+      setModeSelection((prevStates) => ({
+        Practice: true,
+        Tournament: false,
+        UnPublic: false,
+      }));
+    }
+  };
+
   useEffect(() => {
     setMessage("");
     setIsFailed(false);
@@ -63,7 +99,7 @@ export default function CreateQuestion() {
             className="border-2 border-stone-950 rounded-md p-1"
           >
             <option value={""}>---please select category---</option>
-            <option value={"General Skill"}>General Skill</option>
+            <option value={1}>General Skill</option>
             <option value={"Cryptography"}>Cryptography</option>
             <option value={"Network"}>Network</option>
             <option value={"Forensics"}>Forensics</option>
@@ -80,16 +116,42 @@ export default function CreateQuestion() {
             <option value={"Hard"}>Hard</option>
           </select>
           <br />
-          <label>Mode</label>
-          <select
-            name="type"
-            className="border-2 border-stone-950 rounded-md p-1"
-          >
-            <option value={""}>---please select Mode---</option>
-            <option value={"Practice"}>Practice</option>
-            <option value={"Tournament"}>Tournament</option>
-          </select>
-          <br />
+          <div>
+            <p>Mode</p>
+            {Object.keys(modeSelection).map((buttonKey, i) => (
+              <button
+                key={i}
+                type="button"
+                className={`px-4 py-2 font-bold rounded transition ${
+                  modeSelection[buttonKey]
+                    ? "bg-green-500 text-white"
+                    : "bg-red-500 text-white"
+                }`}
+                onClick={() => handleToggle(buttonKey)}
+              >
+                {buttonKey}: {modeSelection[buttonKey] ? "ON" : "OFF"}
+              </button>
+            ))}
+            {/* <button
+              type="button"
+              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              Practice
+            </button>
+            <button
+              type="button"
+              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              Tournament
+            </button>
+
+            <button
+              type="button"
+              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              None
+            </button> */}
+          </div>
           <label>Description</label>
           <textarea
             name="Description"
