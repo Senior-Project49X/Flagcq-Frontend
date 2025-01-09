7300b1fd41714bb3f63039c3406958dfb25abb72
nunu111 <nunu111za@hotmail.com>
2025-01-10 02:15:28 +0700

    fix navbar not stcky

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar({ point }: Readonly<NavbarProps>) {
     // Add more links here as needed
   ];
   return (
-    <nav className="bg-[#090147] py-4 px-8  sticky">
+    <nav className="bg-[#090147] py-4 px-8 absolute sticky top-0 w-full z-50">
       <div className="flex justify-between items-center">
         {/* Left side: Logo */}
         <div>
