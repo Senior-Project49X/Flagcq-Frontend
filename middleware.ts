6280d6ebe80982443a731855aa9a935905c5bf4f
Sirawich <Sirawich_Pin@cmu.ac.th>
2025-01-13 12:21:55 +0700

    merge confilct

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,38 @@ import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
 // 1. Specify protected and public routes
-const protectedRoutes = ['/','/tournament','/leaderboard','/createQuestion','/profile']
-const adminRoutes = ['/createQuestion']
-const publicRoutes = ['/login', '/cmuOAuthCallback' ]
+const protectedRoutes = [
+  "/",
+  "/tournament",
+  "/leaderboard",
+  "/createQuestion",
+  "/profile",
+];
+const adminRoutes = ["/createQuestion", "/tournament/CreateTour"];
+const publicRoutes = ["/login", "/cmuOAuthCallback"];
 
 const ip = process.env.NEXT_PUBLIC_IP_URL;
 export const CheckToken = async (token: string | undefined) => {
   try {
-    const resp = await axios.post(`${ip}/api/token`,{token});
+    const resp = await axios.post(`${ip}/api/token`, { token });
 
     return {
       tokenValid: true,
       role: resp.data.role,
     }; // Return the points
   } catch (e) {
-
-    return { tokenValid: false } // Return a fallback value in case of error
-
+    return { tokenValid: false }; // Return a fallback value in case of error
   }
 };
 export default async function middleware(req: NextRequest) {
   // 2. Check if the current route is protected or public
 
-  const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
-  const isPublicRoute = publicRoutes.includes(path)
-  const isAdminRoute = adminRoutes.includes(path)
-  const token = req.cookies.get('cmu-oauth-token')?.value;
-  const {  tokenValid, role } = await CheckToken(token)
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+  const isAdminRoute = adminRoutes.includes(path);
+  const token = req.cookies.get("cmu-oauth-token")?.value;
+  const { tokenValid, role } = await CheckToken(token);
 
   // 4. Redirect to /login if the user is not authenticated
   if (isProtectedRoute && !tokenValid) {
@@ -37,8 +41,8 @@ export default async function middleware(req: NextRequest) {
   }
 
   // Check if the current route is an admin route and the user is not an admin
-  if (isAdminRoute && role !== 'Admin' && role !== 'Ta') {
-    return NextResponse.redirect(new URL('/unauthorized', req.nextUrl))
+  if (isAdminRoute && role !== "Admin" && role !== "Ta") {
+    return NextResponse.redirect(new URL("/unauthorized", req.nextUrl));
   }
 
   // 5. Redirect to /dashboard if the user is authenticated
@@ -47,14 +51,13 @@ export default async function middleware(req: NextRequest) {
   }
 
   if (tokenValid && role) {
-    let response = NextResponse.next()
+    let response = NextResponse.next();
     // Set the cookie in the response
-    response.cookies.set('user-role', role, {  secure: true})
-    
-    return response
-  }
-  return NextResponse.next()
+    response.cookies.set("user-role", role, { secure: true });
 
+    return response;
+  }
+  return NextResponse.next();
 }
 
 // Routes Middleware should not run on
